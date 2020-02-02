ee14c590709cdf4dcddfd4c3873552af3d36aeb3
Lorenzo <lorenzoperezga@gmail.com>
2020-02-02 23:55:11 +0100

    Empiezo con la lectura de ficheros

--- bienestar_api/app/Http/Controllers/FileController.php
+++ bienestar_api/app/Http/Controllers/FileController.php
@@ -6,5 +6,11 @@ use Illuminate\Http\Request;
 
 class FileController extends Controller
 {
-    //
+    public function listApps($user){
+        $response = array('state' => 500, 'response'=>'');
+
+    }
+    public function post(Request $r){
+
+    }
 }
